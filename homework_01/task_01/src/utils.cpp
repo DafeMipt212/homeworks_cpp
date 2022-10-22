22da209a76dd2a56824dcbf41dbe19444018dfa6
IlyaV <akolzin.ilya@mail.ru>
2022-10-22 08:55:10 +0000

    task 1 done

--- homework_01/task_01/src/utils.cpp
+++ homework_01/task_01/src/utils.cpp
@@ -2,6 +2,36 @@
 
 #include <stack>
 
-std::vector<std::string> SplitString(const std::string& data) {
-  return {data};
+/*std::vector<std::string> SplitString(const std::string& data) {
+  return {};
+}*/
+
+std::vector<std::string> SplitString(const std::string &str){
+    std::vector<char> braces;
+    std::vector<std::string> res;
+    std::string temp;
+    for (auto c : str){
+        if ((c!=' ') and (c!='\t')){
+            temp+=c;
+        }
+        else if (braces.size()!=0){
+            temp+=c;
+        }
+        else if (!temp.empty()){
+            res.push_back(temp);
+            temp.clear();
+        }
+        if (c == '('){
+            braces.push_back(c);
+        }
+        else if ((braces.size()>0) && (c == ')')){
+            if (braces.back() == '('){
+                braces.pop_back();
+            }
+        }
+    }
+    if (!temp.empty()){
+        res.push_back(temp);
+    }
+    return res;
 }
