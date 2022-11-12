70246a4b10b14ede6da20a6eeaa60aec8ced42de
0NeR <ner8650@gmail.com>
2022-11-12 08:54:43 +0000

    first commit

--- homework_01/task_01/src/utils.cpp
+++ homework_01/task_01/src/utils.cpp
@@ -1,7 +1,40 @@
-#include "utils.hpp"
+#include <string>
+#include <string_view>
+#include <vector>
 
-#include <stack>
-
-std::vector<std::string> SplitString(const std::string& data) {
-  return {};
-}
+std::vector<std::string> SplitString(const std::string &data) {
+  std::vector<std::string> words;
+  std::string word = "";
+  int check_int = 0;
+  for (char const &symbol : data) {
+    if (check_int == 0) {
+      if (symbol == '(') {
+        check_int = 1;
+      } else if (symbol != ' ' and symbol != '\t') {
+        word.push_back(symbol);
+      } else if (symbol != ' ' or symbol == '\t') {
+        if (!word.empty()) {
+          words.push_back(word);
+          word.clear();
+        }
+      };
+    };
+    if (check_int == 1) {
+      if (symbol != ')') {
+        word.push_back(symbol);
+      } else if (symbol == ')') {
+        word.push_back(symbol);
+        if (!word.empty()) {
+          words.push_back(word);
+          word.clear();
+        }
+        check_int = 0;
+      };
+    };
+  };
+  if (!word.empty()) {
+    words.push_back(word);
+    word.clear();
+  }
+  return {words};
+};
--- homework_01/task_02/src/utils.cpp
+++ homework_01/task_02/src/utils.cpp
@@ -1,7 +1,28 @@
 #include "utils.hpp"
-
+#include <iostream>
 #include <stack>
 
-int Calculate(const std::string& data) {
-  return 0;
+int Calculate(const std::string &data) {
+  char element_a = data[0];
+
+  int int_a = int(element_a);
+
+  char action = data[1];
+
+  char element_b = data[2];
+
+  int int_b = int(element_b);
+
+  if (action == '+') {
+    return element_a + element_b;
+  }
+  if (action == '-') {
+    return element_a - element_b;
+  }
+  if (action == '/') {
+    return element_a / element_b;
+  }
+  if (action == '*') {
+    return element_a * element_b;
+  }
 }
