c361578957407af48c4eccee43b1fc2ac4438d5f
Mikisheva Maria <mikisevam@gmail.com>
2022-11-19 09:08:23 +0000

    feature first homework

--- homework_01/task_02/src/utils.cpp
+++ homework_01/task_02/src/utils.cpp
@@ -1,7 +1,37 @@
 #include "utils.hpp"
-
 #include <stack>
+#include <iostream>
+using namespace std;
 
-int Calculate(const std::string& data) {
-  return 0;
+
+int Calculate(const std::string& data) 
+{
+  char oper;
+  float num1, num2;
+  cout << "Enter an expression like 'number-operation-number' without spaces"<< endl;
+  cin >> num1 >> oper >>  num2;
+  switch (oper) {
+      case '+':
+        cout << num1 + num2 << endl;
+        break;
+
+      case '-':
+        cout << num1 - num2 << endl;
+        break;
+
+      case '*':
+        cout << num1 * num2 << endl;
+        break;
+
+      case '/':
+        cout << num1 / num2 << endl;;
+        break;
+
+      default:
+        cout << "Error! operator is not correct";
+        break;
+    }
+
+    return 0;
+
 }
