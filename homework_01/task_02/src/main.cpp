05613cf2f9c204f71232023972d389d56aecd133
Artem <artrussushkof@gmail.com>
2022-11-23 16:12:04 +0000

    final homework 1

--- homework_01/task_02/src/main.cpp
+++ homework_01/task_02/src/main.cpp
@@ -1,32 +1,22 @@
-#include <iostream>
-#include <stack>
-#include <vector>
 #include <string.h>
-#include <string>
-#include <utils.hpp>
+
+#include <iostream>
 #include <regex>
+#include <stack>
+#include <utils.hpp>
 using namespace std;
 int main() {
-  /*std::string data;
-  std::getline(std::cin, data);
-  std::cout << Calculate(data);
-  return 0;*/
- //eherh832759328623626
-string input;
-int res;
-while(7){
-  
-  std::cout << "Vvedite, poghaluista, stroku: " << std::endl;
-  getline(cin,input);
-  if (input=="quit")
-  {
-    break;
+  string input;
+  int res;
+  cout << "REZULTAT" << Calculate("5*9-8") << endl;
+  while (7) {
+    std::cout << "Vvedite, poghaluista, stroku: " << std::endl;
+    getline(cin, input);
+    if (input == "quit") {
+      break;
+    }
+    cout << Parce(input) << endl;
+    cout << "Double: " << IsDouble(input) << endl;
   }
-  cout<<Parce(input)<<endl;
-  cout<<"Double: "<<IsDouble(input)<<endl;
-  //res=SplitString(s);
-  //cout<<Calculate(input)<<endl;
-}
-return 0;
-
+  return 0;
 }
--- homework_01/task_02/src/utils.cpp
+++ homework_01/task_02/src/utils.cpp
@@ -1,163 +1,190 @@
 #include "utils.hpp"
 
-#include <iostream>
-#include <stack>
-#include <vector>
 #include <string.h>
-#include <string>
+
+#include <iostream>
 #include <regex>
+#include <stack>
+#include <string>
+#include <vector>
 using namespace std;
 
 std::vector<std::string> SplitString(const std::string& data) {
-  std::vector <std::string> v;
-  std::string buff="";
-  bool flag=false;
-  cout<<size(data)<<endl;
-  for (int i=0;i<size(data);++i)
-  {
-    if (data[i]=='(')
-    {
-      flag=true;
+  std::vector<std::string> v;
+  std::string buff = "";
+  bool flag = false;
+  for (int i = 0; i < size(data); ++i) {
+    if (data[i] == '(') {
+      flag = true;
+    } else if (data[i] == ')') {
+      flag = false;
     }
-    else if (data[i]==')')
-    {
-      flag=false;
+    if (((data[i] != ' ') and (data[i] != '\t') and (data[i] != '+') and
+         (data[i] != '-') and (data[i] != '*') and (data[i] != '/')) or
+        (flag == true)) {
+      buff += data[i];
     }
-    if (((data[i]!=' ') and (data[i]!='\t')) or (flag==true))
-    {
-      buff+=data[i];
-    }
-    if (flag==false)
-    {
-      if ((data[i]==' ') or (data[i]=='\t') or (i+1>=size(data)))
-      {
-        if (buff!="")
-        {
+    if (flag == false) {
+      if ((data[i] == ' ') or (data[i] == '\t') or (data[i] == '+') or
+          (data[i] == '-') or (data[i] == '*') or (data[i] == '/') or
+          (i + 1 >= size(data))) {
+        if (buff != "") {
           v.push_back(buff);
-          buff="";
+          buff = "";
+          if ((data[i] == '+') or (data[i] == '-') or (data[i] == '*') or
+              (data[i] == '/')) {
+            buff += data[i];
+            v.push_back(buff);
+            buff = "";
+          }
         }
       }
-    }
-    else 
-        {
-      if (i+1>=size(data))
-      {
-        if (buff!="")
-        {
+    } else {
+      if (i + 1 >= size(data)) {
+        if (buff != "") {
           v.push_back(buff);
-          buff="";
+          buff = "";
         }
       }
     }
   }
-  //std::cout << "Size of vector: "<<v.size() << "\n";
   return v;
 }
 
-bool IsInt_bad_version(const std::string& str)
-{
-  int a=1;
-  bool flag=true;
-  if (str.empty()) flag= false;
-  for (size_t i=1;i<str.size();++i)
-{
-  if ((str[i]>='0') && (str[i]<='9'))/* if (std::isdigit(str[i]))*/a=1;
-  else flag = false;
+bool IsInt_bad_version(const std::string& str) {
+  int a = 1;
+  bool flag = true;
+  if (str.empty()) flag = false;
+  for (size_t i = 1; i < str.size(); ++i) {
+    if ((str[i] >= '0') && (str[i] <= '9'))
+      a = 1;
+    else
+      flag = false;
 
-  if ((!std::isdigit(str[0])) && (str[0]!='-') && (str[0]!='+'))
-  {
-    flag = false;
+    if ((!std::isdigit(str[0])) && (str[0] != '-') && (str[0] != '+')) {
+      flag = false;
+    }
   }
-}
-return flag;
-}
-
-bool IsInt(const std::string& str)
-{
-  static std::regex r(R"([+-]?\d+)"); // проверяем число ли это в системе regular expressions (regex)
-  return std::regex_match(str,r);
+  return flag;
 }
 
-bool IsDouble(const std::string& str)
-{
-  static std::regex r(R"([+-]?(\d*\.)?\d+([Ee][+-]?\d+)?)"); // проверяем double число ли это в системе regular expressions (regex)
-  return std::regex_match(str,r);
+bool IsInt(const std::string& str) {
+  static std::regex r(R"([+-]?\d+)");  // проверяем число ли это в системе
+                                       // regular expressions (regex)
+  return std::regex_match(str, r);
+}
+
+bool IsDouble(const std::string& str) {
+  static std::regex r(
+      R"([+-]?(\d*\.)?\d+([Ee][+-]?\d+)?)");  // проверяем double число ли это в
+                                              // системе regular expressions
+                                              // (regex)
+  return std::regex_match(str, r);
 }
 
 string Parce(const std::string& input) {
-  bool Int_test=IsInt(input);
-  if (Int_test) {return "true";
-    std:cout<<std::stod(input)<<std::endl;
-  }
-  else return "false";
+  bool Int_test = IsInt(input);
+  if (Int_test) {
+    return "true";
+  std:
+    cout << std::stod(input) << std::endl;
+  } else
+    return "false";
 }
 
-int Calculate(const std::string& data)  {
-int i=0;
-int buff_int=0;
-int znak;
-int rez=0;
-vector<int> chisla;
-vector<string> r= SplitString(data);
-for (int i=0;i<r.size();++i) {
-    std::cout << r[i] << '\n';
-}
-for (i=0;i<data.size();++i)
-{
-  if ((data[i]>='0') && (data[i]<='9'))
-  {
-  
+int Calculate(const std::string& data) {
+  int i = 0;
+  int buff_int = 0;
+  std::string znak;
+  int rez = 0;
+  // std::string buff_str;
+  vector<int> chisla;
+  vector<string> r = SplitString(data);
+  for (int i = 0; i < r.size(); ++i) {
+    std::cout << i << "element: " << r[i] << '\n';
   }
-for (i=0;i<r.size();++i)
-{
-//if((r[i]!="-") and (r[i]!="+") and (r[i]!="/") and (r[i]!="*")){
-  //chisla.push_back(stoi(r[i]));
-//}
-/*else if (r[i]=="+") {
-znak=0;
-}
-else if (r[i]=="-") {
-znak=1;
-}
-else if (r[i]=="/") {
-znak=2;
-}
-else if (r[i]=="*") {
-znak=3;
-}*/
-}
-if (!chisla.empty()) rez=chisla[0];
-else
-{
-  cout<<"Error"<<endl;
-  rez=-1;
-}
-cout<<znak<<endl;
-if (!chisla.empty()){
-for(i=1;i<chisla.size();++i)
-{
-  if (znak==0)
-  {
-    rez+=chisla[i];
+  for (i = 0; i < data.size(); ++i) {
+    if ((data[i] >= '0') && (data[i] <= '9')) {
+    }
+    for (i = 0; i < r.size(); ++i) {
+      if ((r[i] != "+") and (r[i] != "-") and (r[i] != "*") and (r[i] != "/")) {
+        chisla.push_back(stoi(r[i]));
+      } else {
+        znak = r[i];
+      }
+    }
+    if (!chisla.empty())
+      rez = chisla[0];
+    else {
+      cout << "Error" << endl;
+      rez = -1;
+    }
+    cout << znak << endl;
+    if (!chisla.empty()) {
+      for (i = 1; i < chisla.size(); ++i) {
+        if (znak == "+") {
+          rez += chisla[i];
+        }
+        if (znak == "-") {
+          rez -= chisla[i];
+        }
+        if (znak == "*") {
+          rez *= chisla[i];
+        }
+        if (znak == "/") {
+          rez /= chisla[i];
+        }
+      }
+    }
+    return rez;
   }
-  if (znak==1)
-  {
-    rez-=chisla[i];
-  }
-  if (znak==2)
-  {
-    rez/=chisla[i];
-  }
-  if (znak==3)
-  {
-    rez*=chisla[i];
-  }
-}
-}
-return rez;
-}
 }
 
-
-
-
+int CalculatePrefix(const std::string& data) {
+  cout << "Not Ready" << endl;
+  int i = 0;
+  int buff_int = 0;
+  std::string znak;
+  int rez = 0;
+  // std::string buff_str;
+  vector<int> chisla;
+  vector<string> r = SplitString(data);
+  for (int i = 0; i < r.size(); ++i) {
+    std::cout << i << "element: " << r[i] << '\n';
+  }
+  for (i = 0; i < data.size(); ++i) {
+    if ((data[i] >= '0') && (data[i] <= '9')) {
+    }
+    for (i = 0; i < r.size(); ++i) {
+      if ((r[i] != "+") and (r[i] != "-") and (r[i] != "*") and (r[i] != "/")) {
+        chisla.push_back(stoi(r[i]));
+      } else {
+        znak = r[i];
+      }
+    }
+    if (!chisla.empty())
+      rez = chisla[0];
+    else {
+      cout << "Error" << endl;
+      rez = -1;
+    }
+    cout << znak << endl;
+    if (!chisla.empty()) {
+      for (i = 1; i < chisla.size(); ++i) {
+        if (znak == "+") {
+          rez += chisla[i];
+        }
+        if (znak == "-") {
+          rez -= chisla[i];
+        }
+        if (znak == "*") {
+          rez *= chisla[i];
+        }
+        if (znak == "/") {
+          rez /= chisla[i];
+        }
+      }
+    }
+    return rez;
+  }
+}
