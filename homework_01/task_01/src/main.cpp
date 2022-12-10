ec03ec4b46ded7f67011f37a745ff400d057fbfd
arzaaam <arzamastseva.ea@phystech.edu>
2022-12-10 13:11:20 +0300

    homework

--- homework_01/task_01/src/main.cpp
+++ homework_01/task_01/src/main.cpp
@@ -3,7 +3,9 @@
 #include <utils.hpp>
 
 int main() {
-  for (const auto& word : SplitString("asdas das das fgag (adasd 1fas)")) {
+  for (const auto& word : SplitString(
+      "asdas das das fgag (adasd 1fas)")
+  ) {
     std::cout << word << "\n";
   }
   return 0;
