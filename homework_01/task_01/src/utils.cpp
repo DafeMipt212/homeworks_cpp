5b48ec83e16b422c45f3bdd1953c916518b24c1a
Tatyana145 <kornienko.tiu@phystech.edu>
2022-12-12 17:06:19 +0300

    дз Корниенко

--- homework_01/task_01/src/utils.cpp
+++ homework_01/task_01/src/utils.cpp
@@ -3,5 +3,24 @@
 #include <stack>
 
 std::vector<std::string> SplitString(const std::string& data) {
-  return {};
+  std::vector<std::string> v;
+  std::string tmp = "";
+
+  for (auto c : s) {
+    if (c != ' ') {
+      tmp += c;
+      continue;
+    }
+
+    if (tmp != "") {
+      v.push_back(tmp);
+      tmp = "";
+    }
+  }
+
+  if (tmp != "") {
+    v.push_back(tmp);
+  }
+
+  return v;
 }
--- homework_01/task_02/src/utils.cpp
+++ homework_01/task_02/src/utils.cpp
@@ -1,7 +1,99 @@
 #include "utils.hpp"
+#include <regex>
+#define vector_string std::vector<std::string>
 
-#include <stack>
+bool is_numb(const std::string &str) {
+    static const std::regex r(R"([+-]?(\d*\.)?\d+([Ee[+-]?\d+)?)");
+    return std::regex_match(str, r);
+}
 
 int Calculate(const std::string& data) {
-  return 0;
+    std::vector<int> v1; // хотела сделать тип double, но в тестах int, так что пока так
+    std::string tmp;
+    bool f = 0, pr = 0, dl = 0, mns = 0;
+    
+    for (size_t i = 0; i < data.size(); ++i) {
+        if (data[i] != "*" && data[i] != "+" && data[i] != "-" && data[i] != "/" && !(is_numb(data[i]))) {
+            std::cout << "ошибка!\n";
+            return 0;
+        }
+
+        if (f) {
+            if (data[i] == ")") {
+                int tmp_ans = Calculate(tmp);
+
+                if (pr) {
+                    v1[v1.size()-1] *= tmp_ans;
+                    pr = 0;
+                } else if (dl) {
+                    v1[v1.size()-1] /= tmp_ans;
+                    dl = 0;
+                } else if (mns) {
+                    v1.push_back(-tmp_ans);
+                    mns = 0;
+                } else {
+                   v1.push_back(tmp_ans);
+                }
+
+                tmp = {};
+                f = 0;
+            } else {
+                tmp.push_back(data[i]);
+            }
+
+            continue;
+        }
+
+        if (data[i] == "(") {
+            f = 1;
+            continue;
+        }
+
+        if (pr) {
+            v1[v1.size()-1] *= std::stod(v[i]);
+            pr = 0;
+            continue;
+        }
+
+        if (dl) {
+            v1[v1.size()-1] /= std::stod(v[i]);
+            dl = 0;
+            continue;
+        }
+
+        if (mns) {
+            v1.push_back(-std::stod(v[i]));
+            mns = 0;
+            continue;
+        }
+
+        if (v[i] == "*") {
+            pr = 1;
+            continue;
+        }
+
+        if (v[i] == "/") {
+            dl = 1;
+            continue;
+        }
+
+        if (v[i] == "-") {
+            mns = 1;
+            continue;
+        }
+
+        if (v[i] == "+") {
+            continue;
+        }
+
+        v1.push_back(std::stod(v[i]));
+    }
+
+    int answer = 0;
+
+    for (auto n : v1) {
+        answer += n;
+    }
+
+    return (int)answer;
 }
