b16d744e55156f742c57f0fade6cb560480df89b
Ivan Khokhlov <lostpointmeister@gmail.com>
2022-12-12 20:09:02 +0000

    remove catch2 test, add gtest

--- homework_01/task_02/src/calculate_test.cpp
+++ homework_01/task_02/src/calculate_test.cpp
@@ -1,24 +1,24 @@
-#include <catch2/catch_test_macros.hpp>
+#include <gtest/gtest.h>
 
 #include "utils.hpp"
 
-TEST_CASE("Calculate", "[simple_sum]") {
-  CHECK(Calculate("1+2") == 3);
-  CHECK(Calculate("2+2") == 4);
-  CHECK(Calculate("1+0") == 1);
-  CHECK(Calculate("0+0") == 0);
+TEST(CalculateTest, simple_sum) {
+  ASSERT_EQ(Calculate("1+2"), 3);
+  ASSERT_EQ(Calculate("2+2"), 4);
+  ASSERT_EQ(Calculate("1+0"), 1);
+  ASSERT_EQ(Calculate("0+0"), 0);
 }
 
-TEST_CASE("Calculate", "[simple_difference]") {
-  CHECK(Calculate("5-2") == 3);
-  CHECK(Calculate("2-2") == 0);
-  CHECK(Calculate("1-0") == 1);
-  CHECK(Calculate("0-0") == 0);
+TEST(CalculateTest, simple_difference) {
+  ASSERT_EQ(Calculate("5-2"), 3);
+  ASSERT_EQ(Calculate("2-2"), 0);
+  ASSERT_EQ(Calculate("1-0"), 1);
+  ASSERT_EQ(Calculate("0-0"), 0);
 }
 
-TEST_CASE("Calculate", "[simple_multiply]") {
-  CHECK(Calculate("5*2") == 10);
-  CHECK(Calculate("2*2") == 4);
-  CHECK(Calculate("1*0") == 0);
-  CHECK(Calculate("0*0") == 0);
+TEST(CalculateTest, simple_multiply) {
+  ASSERT_EQ(Calculate("5*2"), 10);
+  ASSERT_EQ(Calculate("2*2"), 4);
+  ASSERT_EQ(Calculate("1*0"), 0);
+  ASSERT_EQ(Calculate("0*0"), 0);
 }
