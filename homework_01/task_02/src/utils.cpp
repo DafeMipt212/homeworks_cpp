4478790c3352f84a6dcdd7e4c5e0095b1c23b38e
Tatyana145 <kornienko.tiu@phystech.edu>
2022-12-13 06:36:45 +0300

    поменяла названияя переменных, скоро поправлю еще кое-что

--- homework_01/task_02/src/utils.cpp
+++ homework_01/task_02/src/utils.cpp
@@ -9,39 +9,43 @@ bool is_number(const std::string &str) {
 }
 
 int Calculate(const std::string& data) {
-    std::vector<int> v1; // хотела сделать тип double, но в тестах int, так что пока так
+    std::vector<int> terms; // хотела сделать тип double, но в тестах int, так что пока так
     std::string tmp;
-    bool f = 0, pr = 0, dl = 0, mns = 0;
+    std::string number; // пока нигде не используется, скоро будет; исправлю проблему с методом stoi и в целом с логикой
+    bool is_brackets = 0;
+    bool multipl = 0;
+    bool division = 0;
+    bool minus = 0;
     
     for (size_t i = 0; i < data.size(); ++i) {
         if (data[i] != '*' && 
             data[i] != '+' && 
             data[i] != '-' && 
             data[i] != '/' && 
-            !(is_number(std::to_string(data[i])))) {
+            !(is_number(data[i]))) {
             std::cout << "ошибка!\n";
             return 0;
         }
 
-        if (f) {
+        if (is_brackets) {
             if (data[i] == ')') {
                 int tmp_ans = Calculate(tmp);
 
-                if (pr) {
-                    v1[v1.size()-1] *= tmp_ans;
-                    pr = 0;
-                } else if (dl) {
-                    v1[v1.size()-1] /= tmp_ans;
-                    dl = 0;
-                } else if (mns) {
-                    v1.push_back(-tmp_ans);
-                    mns = 0;
+                if (multipl) {
+                    terms[v1.size()-1] *= tmp_ans;
+                    multipl = 0;
+                } else if (division) {
+                    terms[v1.size()-1] /= tmp_ans;
+                    division = 0;
+                } else if (minus) {
+                    terms.push_back(-tmp_ans);
+                    minus = 0;
                 } else {
-                   v1.push_back(tmp_ans);
+                   terms.push_back(tmp_ans);
                 }
 
                 tmp = {};
-                f = 0;
+                is_brackets = 0;
             } else {
                 tmp.push_back(data[i]);
             }
@@ -50,40 +54,40 @@ int Calculate(const std::string& data) {
         }
 
         if (data[i] == '(') {
-            f = 1;
+            is_brackets = 1;
             continue;
         }
 
-        if (pr) {
-            v1[v1.size()-1] *= std::stoi(data[i]);
-            pr = 0;
+        if (multipl) {
+            terms[terms.size()-1] *= std::stoi(data[i]);
+            multipl = 0;
             continue;
         }
 
-        if (dl) {
-            v1[v1.size()-1] /= std::stoi(data[i]);
-            dl = 0;
+        if (division) {
+            terms[terms.size()-1] /= std::stoi(data[i]);
+            division = 0;
             continue;
         }
 
-        if (mns) {
-            v1.push_back(-std::stoi(data[i]));
-            mns = 0;
+        if (minus) {
+            terms.push_back(-std::stoi(data[i]));
+            minus = 0;
             continue;
         }
 
         if (data[i] == '*') {
-            pr = 1;
+            multipl = 1;
             continue;
         }
 
         if (data[i] == '/') {
-            dl = 1;
+            division = 1;
             continue;
         }
 
         if (data[i] == '-') {
-            mns = 1;
+            minus = 1;
             continue;
         }
 
@@ -91,12 +95,12 @@ int Calculate(const std::string& data) {
             continue;
         }
 
-        v1.push_back(std::stoi(data[i]));
+        terms.push_back(std::stoi(data[i]));
     }
 
     int answer = 0;
 
-    for (auto n : v1) {
+    for (auto n : terms) {
         answer += n;
     }
 
