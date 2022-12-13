660244227f8064fc55af84f55eace5d0725694f6
Mr.Snoki <valierii.salnyi@mail.ru>
2022-12-13 20:40:36 +0300

    Complite homework

--- homework_01/task_01/src/utils.cpp
+++ homework_01/task_01/src/utils.cpp
@@ -1,7 +1,39 @@
 #include "utils.hpp"
-
+#include "iostream"
 #include <stack>
 
 std::vector<std::string> SplitString(const std::string& data) {
-  return {};
+  std::vector<std::string> out;
+  size_t start;
+    size_t end = 0;
+    int flag = 0;
+    int begin = 0;
+ 
+    while ((start = data.find_first_not_of(' ', end)) != std::string::npos)
+    {
+      if (data[end]==')'){
+        flag = 0;
+        end += 1;
+        out.push_back(data.substr(begin, end - begin));
+        continue;
+      }
+      if (flag == 1){
+        end += 1;
+        continue;
+      }
+      if (data[start]=='('){
+        begin = start;
+        flag = 1;
+        continue;
+      }
+      if (data[start]=='\t'){
+        start += 1;
+      }
+      end = data.find(' ', start);
+      if (data.find('\t', start) < data.find(' ', start)){
+        end = data.find('\t', start);
+      }
+      out.push_back(data.substr(start, end - start));
+    }
+    return out;
 }
