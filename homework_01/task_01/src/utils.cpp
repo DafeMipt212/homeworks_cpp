19caa9383ea7e545704485266049533377e63b08
half_crazy <Savchik199422@mail.ru>
2022-12-14 19:16:51 +0000

    wip

--- homework_01/task_01/src/utils.cpp
+++ homework_01/task_01/src/utils.cpp
@@ -1,8 +1,34 @@
 #include "utils.hpp"
 
 #include <stack>
+#include <iostream>
+#include <vector>
+using namespace std;
 
-std::vector<std::string> SplitString(const std::string& data) {
-  std::cout<<"hello world";
-  return {data};
+vector <string> SplitString(string& s) {
+    s += " ";
+    string s1 = "";
+    char* ptr = &(s[0]);
+    vector <string> v;
+
+    while(*ptr != '\0'){
+        if ((*ptr == ' ') or (*ptr == '\t')) {
+            if (s1 != "") v.push_back(s1);
+            s1 = "";
+        }
+        else {
+            s1 += *ptr;
+            }
+        ++ptr;
+        }
+    for(int i = 0; i < v.size(); ++i){
+        cout << v[i] << endl;} 
+    return v;
+  //return {data};
+}
+
+int main(){
+    string s;    
+    getline(cin, s);
+    SplitString(s);
 }
