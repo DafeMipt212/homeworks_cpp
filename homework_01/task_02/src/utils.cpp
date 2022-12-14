30d5dd2813fa3a68d2cfb757155b2ad4ad1635a8
half_crazy <Savchik199422@mail.ru>
2022-12-14 23:55:25 +0000

    hihi

--- homework_01/task_02/src/utils.cpp
+++ homework_01/task_02/src/utils.cpp
@@ -2,7 +2,40 @@
 
 #include <stack>
 
-int Calculate(const std::string& data) {
-  std::cout<<"hello world";
-  return 0;
+#include <stack>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int Calculate(string& str) {
+    str += " ";
+    string CurrentString = "";
+    char* ptr = &(str[0]);
+    vector <string> data;
+
+    while(*ptr != '\0'){
+        if ((*ptr == '+') ||(*ptr == '-')||(*ptr == '*')||(*ptr == '/')) {
+            if (CurrentString != ""){ 
+            data.push_back(CurrentString);
+            CurrentString = "";
+            }
+            CurrentString += *ptr;
+            data.push_back(CurrentString);
+            CurrentString = "";
+            ++ptr;
+            }
+
+        CurrentString += *ptr;
+        ++ptr;
+    }
+    data.push_back(CurrentString);
+    
+    int a = stoi(data[0]);
+    int b = stoi(data[2]);
+
+    if (data[1] == "+") {return a + b;}
+    else if (data[1] == "-") {return a - b;}
+    else if (data[1] == "*") {return a * b;}
+    else if (data[1] == "/") {return a / b;}
+    else return 0;
 }
