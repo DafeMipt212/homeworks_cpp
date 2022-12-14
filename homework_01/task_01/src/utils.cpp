e10e177e4091261f4e313a5ed710b67ca9dc9868
Tatyana145 <kornienko.tiu@phystech.edu>
2022-12-14 20:54:45 +0300

    добавила условие со скобками, надо еще доработать

--- homework_01/task_01/src/utils.cpp
+++ homework_01/task_01/src/utils.cpp
@@ -5,8 +5,28 @@
 std::vector<std::string> SplitString(const std::string& data) {
   std::vector<std::string> answer;
   std::string tmp = "";
+  bool is_bracket = 0;
 
   for (auto elem : data) {
+    if (is_bracket) {
+      if (elem == ')') {
+        is_bracket = 0;
+        if (tmp != "") {
+          answer.push_back(tmp);
+          tmp = "";
+        }
+      } else {
+        tmp += elem;
+      }
+
+      continue;
+    }
+
+    if (elem == '(') {
+      is_bracket == 1;
+      continue;
+    }
+
     if (elem != ' ' && elem != '\t') {
       tmp += elem;
       continue;
