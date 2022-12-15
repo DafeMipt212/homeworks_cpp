7971c13515aaa8a5fae80cb51944a3ac7cf6ecf2
lovetea10 <alina.lulushkina@mail.ru>
2022-12-16 00:46:59 +0300

    first commit

--- homework_01/task_01/src/utils.cpp
+++ homework_01/task_01/src/utils.cpp
@@ -1,7 +1,29 @@
 #include "utils.hpp"
-
+#include <iostream>
+#include <sstream>
+#include <algorithm>
 #include <stack>
 
-std::vector<std::string> SplitString(const std::string& data) {
-  return {};
+typedef int typevec;
+typedef std::vector<typevec> ValueVec;
+ 
+ValueVec& SplitString(const std::string &s, char dels, ValueVec&elems)
+{
+    std::stringstream srw(s);
+    std::string item;
+    while(std::getline(srw, item, dels))
+    {
+        elems.push_back(std::stoi(item));
+    }
+    return elems;
 }
+ 
+ValueVec SplitString(const std::string &str, char dels)
+{
+    ValueVec elems;
+    SplitString(str, dels, elems);
+    return elems;
+}
+ 
+
+ 
--- homework_01/task_02/src/utils.cpp
+++ homework_01/task_02/src/utils.cpp
@@ -1,55 +1,11 @@
 #include "utils.hpp"
 #include <stack>
 #include <iostream>
-int power (int c, int d)
-{   int p = c;
-    if (c == 0) {std::cout<<1; return 0;}
-    for (int i=1; i<d; i++)
-    {
-    c*=p;
-    }
-return c;
-}
-int factor (int h)
-{
-    int num=h;
-    for (int i=0; i<num; i++)
-    {
-        h*=--num;
-        }
-    return h;
-}
-int logpro (int a, int c)
- {
-     int S=1;
-     int n=0; 
-     while (a>S)
-     {
-        a = (a/c);
-        n++; 
-         }
-  return n;
- }
-float kor(float A, float x)
-{
-  float xold;
-  do
-  {
-    xold=x;
-    x=(x+A/x)/2.0;
-  } while (x!=xold);
-  return x;
-}
+
+  
 int Calculate(const std::string& data) {
-    int a = 0;
-    int b = 0;
-    char operation;
-    std::cout << "Введите первое число: ";
-    std::cin >> a;
-    std::cout << "Введите второе число: ";
-    std::cin >> b;
-    std::cout << "Введите операцию: ";
-    std::cin >> operation;
+vector <string> data
+    
 
     switch (operation) {
     case '-':
@@ -60,24 +16,6 @@ int Calculate(const std::string& data) {
         break;
     case '*':
         std::cout << "a * b = " << a * b << '\n'<<std::endl;
-        break;
-    case '/':
-        std::cout << "a / b = " << a / b << '\n'<<std::endl;
-        break;
-    case '%':
-        std::cout << "a % b = " << a % b << '\n'<<std::endl;
-        break; 
-    case '&':
-        std::cout << "a & b = " << power(a,b) << '\n'<<std::endl; // '&' возведение в степень
-        break;   
-    case '#':
-        std::cout << "a # b = " << logpro(a,b) << '\n'<<std::endl;// '#' вычисление логарифма по основанию b
-        break;
-    case '@':
-        std::cout << "a @ b = " << kor(a,b) << '\n'<<std::endl;// '@' вычисление корня
-        break;
-    case '!':
-        std::cout << "a ! b = " << factor (a) << '\n'<<std::endl;// '!' факториал числа
         break;
             
     default:
